f9e545996ba4c45e07f9081d4852880413a50e19
KennethEnevoldsen <kennethcenevolsen@gmail.com>
2021-12-05 14:59:22 +0100

    bumped version id

--- asent/about.py
+++ asent/about.py
@@ -1,4 +1,4 @@
 __title__ = "asent"
 
-__version__ = "0.0.2"  # the ONLY source of version ID
+__version__ = "0.0.3"  # the ONLY source of version ID
 __download_url__ = "https://github.com/kennethenevoldsen/asent"
