540259dea7a88d98ed0e4780fef1e9aaa1948cbb
KennethEnevoldsen <kennethcenevolsen@gmail.com>
2021-12-26 00:59:36 +0100

    added version id to init

--- asent/__init__.py
+++ asent/__init__.py
@@ -1,4 +1,6 @@
-from .utils import register_lexicon, lexicons
+from .utils import register_lexicon, lexicons  # noqa
 
-from .visualize import visualize
-from .lang import da, se, no, en, emoji
+from .visualize import visualize  # noqa
+from .lang import da, se, no, en, emoji  # noqa
+
+from .about import __version__, __download_url__, __documentation__, __title__  # noqa
