8e07d9346d4c539db41baac2ac8bbc2b60d4e850
KennethEnevoldsen <kennethcenevolsen@gmail.com>
2021-12-25 16:01:17 +0100

    updated manifest.in

--- asent/about.py
+++ asent/about.py
@@ -1,5 +1,5 @@
 __title__ = "asent"
 
-__version__ = "0.0.4"  # the ONLY source of version ID
+__version__ = "0.0.5"  # the ONLY source of version ID
 __download_url__ = "https://github.com/kennethenevoldsen/asent"
 __documentation__ = "https://kennethenevoldsen.github.io/asent"
