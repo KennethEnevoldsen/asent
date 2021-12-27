f4717f506622c5833b1f39c8c544d5c752a18352
KennethEnevoldsen <kennethcenevolsen@gmail.com>
2021-12-27 11:41:20 +0100

    format to black 🕶

--- asent/tests/test_lexicon.py
+++ asent/tests/test_lexicon.py
@@ -7,7 +7,7 @@ def test_lexicons():
 
     for lexicon in asent.lexicons.get_all():
         print(lexicon)
-    
+
     asent.register_lexicon("my_custom_lexicon", {"happy": 2, "sad": -2})
     lex = asent.lexicons.get("my_custom_lexicon")
     assert isinstance(lex, dict)
