4b484e3607c6711fb9949093031dd712d1817a58
KennethEnevoldsen <kennethcenevolsen@gmail.com>
2022-01-02 22:14:55 +0100

    added dataset validation to benchmark scripts

---
+++ Benchmarks/benchmark.py
@@ -0,0 +1,139 @@
+from typing import Tuple, Iterable, Optional, Union
+
+import numpy as np
+from sklearn.metrics import (
+    f1_score,
+    accuracy_score,
+    r2_score,
+    mean_squared_error,
+)
+
+
+from spacy.language import Language
+import asent
+
+from functools import partial
+
+
+def to_categorical(
+    value: np.ndarray, neutral_range: Tuple[float, float] = (0, 0)
+) -> np.ndarray:
+    """Convert np array to 0, 1, -1 indicating neutral, postive, negative respectively."""
+    v = value.copy()
+
+    # if it is string
+    if isinstance(value[0], str):
+        v = np.zeros(value.shape)
+        value_ = np.array([x.lower() for x in value])
+        v[np.isin(value_, np.array(["pos", "positive", "positiv"]))] = 1
+        v[np.isin(value_, np.array(["neg", "negative", "negativ"]))] = -1
+        v[np.isin(value_, np.array(["neu", "neutral"]))] = 0
+    else:
+        v[(value >= neutral_range[0]) & (value <= neutral_range[1])] = 0
+        v[value < neutral_range[0]] = -1
+        v[value > neutral_range[1]] = 1
+    return v
+
+
+def find_optimal_neutral_range(
+    preds, y_true, range_=[t / 100 for t in range(100)], verbose=False
+):
+    best = ((0, 0), 0)
+    for t in range_:
+        y_pred = to_categorical(np.array(preds), neutral_range=(t, t))
+        f1 = f1_score(y_true, y_pred, average="macro")
+        acc = accuracy_score(y_true, y_pred)
+        if verbose:
+            print(f"[{t}] F1: {f1}, acc: {acc}")
+        if f1 > best[1]:
+            best = ((t, t), f1)
+    return best[0]
+
+
+def show_n_incorrect(
+    nlp: Language,
+    texts: np.ndarray,
+    y_true: np.ndarray,
+    y_pred: np.ndarray,
+    n=1,
+    analysis=False,
+):
+    import random
+
+    w_texts = texts[y_true != y_pred]
+    w_y_true = y_true[y_true != y_pred]
+    w_y_pred = y_pred[y_true != y_pred]
+    wrong_shuffled = [
+        (t, true, pred) for t, true, pred in zip(w_texts, w_y_true, w_y_pred)
+    ]
+    random.shuffle(wrong_shuffled)
+    for i, t in zip(range(n), wrong_shuffled):
+        print(f"True: {t[1]} -  Predicted: {t[2]}")
+        doc = nlp(t[0])
+        asent.visualize(doc)
+        if analysis is True:
+            asent.visualize(doc, style="analysis")
+        print(doc._.polarity)
+
+    return w_texts
+
+
+def validate(
+    texts: Iterable[str],
+    true: Iterable[Union[float, str]],
+    nlp: Language,
+    search_for_best_range: bool = True,
+    categorical: Optional[bool] = None,
+):
+    preds = [doc._.polarity.compound for doc in nlp.pipe(texts)]
+    y_true = np.array(list(true))
+
+    if isinstance(y_true[0], str) and categorical is None:
+        categorical = True
+
+    if categorical is True or categorical is None:
+        c_y_true = to_categorical(np.array(y_true))
+        if search_for_best_range is True:
+            n_range = find_optimal_neutral_range(preds, c_y_true)
+        else:
+            n_range = (0, 0)
+        c_preds = to_categorical(np.array(preds), neutral_range=n_range)
+
+    metrics = {
+        "Macro F1": (partial(f1_score, average="macro"), "categorical"),
+        "Accuracy": (accuracy_score, "categorical"),
+        "RMSE": (mean_squared_error, "Non-categorical"),
+        "R2": (r2_score, "Non-categorical"),
+    }
+
+    output = {}
+    if categorical is None or categorical is True:
+        output["used neutral range"] = n_range
+    for name, m in metrics.items():
+        metric, cat = m
+        if cat == "categorical" and (categorical is None or categorical is True):
+            output[name] = metric(c_y_true, c_preds)
+        if cat != "categorical" and (categorical is None or categorical is False):
+            output[name] = metric(y_true, np.array(preds))
+    return output
+
+
+def validate_datasets(nlp, datasets, verbose: bool = True):
+    output = {}
+    for d_name, loader in datasets.items():
+        texts, true = loader()
+
+        out = validate(texts, true, nlp)
+        output[d_name] = out
+
+        if verbose:
+            print(d_name)
+            print("\t", end="")
+
+            [
+                print(f"{name}: {score:.2f} - ", end="")
+                for name, score in out.items()
+                if not isinstance(score, tuple)
+            ]
+            print(f"\tUsed neutral-range: {out['used neutral range']}")
+    return out
--- Benchmarks/benchmark_da.py
+++ Benchmarks/benchmark_da.py
@@ -3,19 +3,19 @@ A benchmark script for the Danish pipelines
 """
 
 import sys
+from typing import Callable, Dict
+
 
 sys.path.append(".")
-import os
-os.chdir("..")
+sys.path.append("benchmark/")
+sys.path.append("..")
 
-from typing import Iterable, List, Tuple
 
-import numpy as np
+from benchmark import validate_datasets, show_n_incorrect
+
 import asent
 import spacy
-from spacy.language import Language
 
-from sklearn.metrics import f1_score, confusion_matrix, accuracy_score
 
 da_components = [
     c for c in asent.components.get_all().keys() if c.split("_")[1] == "da"
@@ -32,111 +32,106 @@ def load_europarl1():
 
 
 def load_europarl2():
-    from danlp.datasets import EuroparlSentiment2
+    from datasets import load_dataset
 
-    eurosent = EuroparlSentiment2()
-
-    df = eurosent.load_with_pandas()
-    return df["text"], df["polarity"]
+    dataset = load_dataset("DDSC/europarl")
+    ds = dataset["train"]
+    return ds["text"], ds["label"]
 
 
 def load_lccsent():
-    from danlp.datasets import LccSentiment
+    from datasets import load_dataset
 
-    lccsent = LccSentiment()
+    dataset = load_dataset("DDSC/lcc")
+    ds = dataset["train"]
+    return ds["text"], ds["label"]
 
-    df = lccsent.load_with_pandas()
-    return df["text"], df["valence"]
+
+def load_angrytweets():
+    from datasets import load_dataset
+
+    dataset = load_dataset("DDSC/angry-tweets")
+    ds = dataset["train"]
+    return ds["text"], ds["label"]
+
+
+def load_twittersent():
+    from datasets import load_dataset
+
+    dataset = load_dataset("DDSC/twitter-sent")
+    ds = dataset["train"]
+    return ds["text"], ds["label"]
 
 
 danish_datasets = {
     "europarl1": load_europarl1,
     "europarl2": load_europarl2,
     "lccsent": load_lccsent,
+    "angry-tweets": load_angrytweets,
+    "twitter-sent": load_twittersent,
 }
 
+c = da_components[0]
+nlp = spacy.load("da_core_news_lg")
+nlp.add_pipe(c, config={"force": True})
 
-def to_pos_neg_neutral(
-    value: np.ndarray, neutral_range: Tuple[float, float] = (0, 0)
-) -> np.ndarray:
-    """Convert np array to 0, 1, -1 indicating neutral, postive, negative respectively."""
-    v = value.copy()
+validate_datasets(nlp, danish_datasets)
 
-    # if it is string
-    if isinstance(value[0], str):
-        v = np.zeros(value.shape)
-        value_ = np.array([x.lower() for x in value])
-        v[np.isin(value_, {"pos", "positive"})] = 1
-        v[np.isin(value_, {"neg", "negative"})] = -1
-        v[np.isin(value_, {"neu", "neutral"})] = 0
-    else:
-        v[(value >= neutral_range[0]) & (value <= neutral_range[1])] = 0
-        v[value < neutral_range[0]] = -1
-        v[value > neutral_range[1]] = 1
-    return v
+from asent.getters import make_valance_getter, make_is_negated_getter, make_is_negation_getter, make_intensifier_getter
+asent.lexicons.get("lexicon_da_v1")
 
+da_getters = {} 
 
-def find_optimal_neutral_range(preds, y_true, range_ = [t/100 for t in range(100)], verbose=False):
-    best = ((0, 0), 0)
-    for t in range_:
-        y_pred = to_pos_neg_neutral(np.array(preds), neutral_range = (t, t))
-        f1 = f1_score(y_true, y_pred, average="macro")
-        acc = accuracy_score(y_true, y_pred)
-        if verbose:
-            print(f"[{t}] F1: {f1}, acc: {acc}")
-        if f1 > best[1]:
-            best = ((t, t), f1)
-    return best 
+def make_valence_getters():
+    getters = {}
+    getters["valence_standard"] = make_valance_getter(lexicon=asent.lexicons.get("lexicon_da_v1"), lemmatize=True, lowercase=True, cap_differential=True)
+    getters["valence_no_cap"] = make_valance_getter(lexicon=asent.lexicons.get("lexicon_da_v1"), lemmatize=True, lowercase=True, cap_differential=False)
+    getters["valence_no_lemma"] = make_valance_getter(lexicon=asent.lexicons.get("lexicon_da_v1"), lemmatize=False, lowercase=True, cap_differential=False)
 
+    getters["standard_valence"] = make_valance_getter(lexicon=asent.lexicons.get("lexicon_da_afinn"), lemmatize=True, lowercase=True, cap_differential=True)
+    getters["valence_no_cap"] = make_valance_getter(lexicon=asent.lexicons.get("lexicon_da_afinn"), lemmatize=True, lowercase=True, cap_differential=False)
+    getters["valence_no_lemma"] = make_valance_getter(lexicon=asent.lexicons.get("lexicon_da_afinn"), lemmatize=False, lowercase=True, cap_differential=False)
 
-for name, loader in danish_datasets.items():
-    texts, sent = loader()
+    # sentida1
 
-    # setup asent pipeline
-    c = da_components[0]
-    nlp = spacy.load("da_core_news_lg")
-    nlp.add_pipe(c, config={"force": True})
+def make_is_negated_getters():
+    getters = {}
+    neg = make_is_negation_getter(negations=asent.lexicons.get("negations_da_v1"), lemmatize=True, lowercase=True)
+    getters["is_negated_standard"] = make_is_negated_getter(is_negation_getter=neg)
+    neg = make_is_negation_getter(negations={}, lemmatize=True, lowercase=True)
+    getters["is_negated_no_negations"] = make_is_negated_getter(is_negation_getter=neg)
+    
+    neg = ...
+    getters["is_negated_with_morph_neg"] = make_is_negated_getter(is_negation_getter=neg)
 
-    preds = [doc._.polarity.compound for doc in nlp.pipe(texts)]
+    neg = ...
+    getters["is_negated_with_dep_neg"] = make_is_negated_getter(is_negation_getter=neg)
+
+    neg = ...
+    getters["is_negated_with_dep_and_morph"] = make_is_negated_getter(is_negation_getter=neg)
 
 
-    y_true = to_pos_neg_neutral(np.array(sent))
-    best_range = find_optimal_neutral_range(preds, y_true, verbose=True)
-    y_pred = to_pos_neg_neutral(np.array(preds), neutral_range=best_range[0])
-    conf = confusion_matrix(y_true, y_pred)
-    f1 = f1_score(y_true, y_pred, average="macro")
-    acc = accuracy_score(y_true, y_pred)
 
-    print(f"Dataset: {name}")
-    print(f"\tAccuracy: {acc:.2f} - Macro F1: {f1:.2f} - Range: {best_range[0]}")
+def make_is_intensifiers_getters():
+    getters = {}
+    getters["intensifier_standard"] = make_intensifier_getter(intensifiers=asent.lexicons("intensifiers_da_v1"), lemmatize=True)
+    getters["intensifier_no_cap_diff"] = make_intensifier_getter(intensifiers=asent.lexicons("intensifiers_da_v1"), lemmatize=True, cap_differential=False)
+    getters["intensifier_no_intensifiers"] = make_intensifier_getter(intensifiers={})
+    # getters["intensifier_with_dep_lemma"] = ...
+    # getters["intensifier_with_dep_no_lemma"] = ...
 
+def make_span_polarity_getters():
+    # with but
+    # without but
 
-def show_n_incorrect(
-    nlp: Language, texts: np.ndarray, y_true: np.ndarray, y_pred: np.ndarray, n=1, analysis=False
-):
-    import random
-
-    w_texts = texts[y_true != y_pred]
-    w_y_true = y_true[y_true != y_pred]
-    w_y_pred = y_pred[y_true != y_pred]
-    wrong_shuffled = [
-        (t, true, pred) for t, true, pred in zip(w_texts, w_y_true, w_y_pred)
-    ]
-    random.shuffle(wrong_shuffled)
-    for i, t in zip(range(n), wrong_shuffled):
-        print(f"True: {t[1]} -  Predicted: {t[2]}")
-        doc = nlp(t[0])
-        asent.visualize(doc)
-        if analysis is True:
-            asent.visualize(doc, style="analysis")
-        print(doc._.polarity)
-
-    return w_texts
+    # with without exclamation
+    # with without questionmarks
+    
 
 
-texts, sent = danish_datasets["europarl2"]()
-preds = [doc._.polarity.compound for doc in nlp.pipe(texts)]
+def grid_search(nlp, getters = Dict[str, Callable]):
+    pass
 
-y_true = to_pos_neg_neutral(np.array(sent))
-y_pred = to_pos_neg_neutral(np.array(preds))
-incorrect = show_n_incorrect(nlp, texts=texts, y_true=y_true, y_pred=y_pred)
+# texts, pred = danish_datasets["twitter-sent"]()
+
+# show_n_incorrect(nlp, texts, pred)
