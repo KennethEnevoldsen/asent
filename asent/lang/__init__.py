d22d6424504ac17bec67c67050c52a00972e0d72
KennethEnevoldsen <kennethcenevolsen@gmail.com>
2022-06-28 11:39:47 +0200

    added test for german

--- asent/lang/__init__.py
+++ asent/lang/__init__.py
@@ -1,3 +1,4 @@
+from .autoconstructed import create_xx_sentiment_component  # noqa
 from .da import create_da_sentiment_component  # noqa
 from .emoji import LEXICON  # noqa
 from .en import create_en_sentiment_component  # noqa
